caa1fdd51922558abd1e2ebb14fa3b7dbf857d17
Akirami <66513481+A-kirami@users.noreply.github.com>
2025-04-13 21:20:29 +0800

    🥅 捕获广播中出现的错误

--- nonebot_plugin_broadcast/__init__.py
+++ nonebot_plugin_broadcast/__init__.py
@@ -1,4 +1,4 @@
-from nonebot import on_command
+from nonebot import logger, on_command
 from nonebot.adapters.onebot.v11 import Bot, Message, PrivateMessageEvent
 from nonebot.params import CommandArg
 from nonebot.permission import SUPERUSER
@@ -10,7 +10,12 @@ broadcast = on_command("广播", aliases={"bc"}, permission=SUPERUSER)
 async def _(bot: Bot, event: PrivateMessageEvent, arg: Message = CommandArg()) -> None:
     msg = arg.extract_plain_text().strip()
     if not msg:
-        await bot.send_private_msg(user_id=event.user_id, message="请在指令后接需要广播的消息")
+        await bot.send_private_msg(
+            user_id=event.user_id, message="请在指令后接需要广播的消息"
+        )
     group_list = await bot.get_group_list()
     for group in group_list:
-        await bot.send_group_msg(group_id=group["group_id"], message=msg)
+        try:
+            await bot.send_group_msg(group_id=group["group_id"], message=msg)
+        except Exception as e:
+            logger.warning(f"群 {group} 广播消息时出错: {e}")
